cf17129c46eebc4fc248e1e1b8caaf3da0d9772f
tpals5236-art <tpals5236@gmail.com>
2026-05-26 22:02:12 +0900

    첫 테스트 구현

--- src/bpe.py
+++ src/bpe.py
@@ -43,7 +43,17 @@ class BPETokenizer:
         1. 특수 토큰 4개를 고정 ID 0~3에 등록합니다.
         2. byte 0~255를 ID 4~259에 bytes([byte_value]) 형태로 등록합니다.
         """
-        raise NotImplementedError("_init_special_tokens를 구현하세요.")
+        for i, token in enumerate(SPECIAL_TOKENS):  #enumerate -> 리스트에서 값을 꺼낼 때 번호표를 붙여서 꺼내는 매서드
+            self.id_to_token[i] = token
+            self.token_to_id[token] = i
+
+        for byte_value in range(NUM_BYTES):
+            self.id_to_token[BYTE_OFFSET + byte_value] = bytes([byte_value])
+            self.token_to_id[bytes([byte_value])] = BYTE_OFFSET + byte_value
+
+
+
+
 
     def get_pad_id(self):
         """padding 토큰 ID."""
